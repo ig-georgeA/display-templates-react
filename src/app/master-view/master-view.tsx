cddccffe171366522e2a7408d90458f115bc05af
George Abraham <41584300+ig-georgeA@users.noreply.github.com>
2025-06-11 14:11:13 -0400

    display template initial version

---
+++ src/app/master-view/master-view.tsx
@@ -0,0 +1,64 @@
+import { IgrCellTemplateContext, IgrColumn, IgrGrid, IgrGridModule } from '@infragistics/igniteui-react-grids';
+import { useGetEmployees } from '../hooks/northwind-hooks';
+import '@infragistics/igniteui-react-grids/grids/combined.js';
+import styles from './master-view.module.css';
+import createClassTransformer from '../style-utils';
+import '/src/app/base-view-styles.css';
+
+IgrGridModule.register();
+
+export default function MasterView() {
+  const classes = createClassTransformer(styles);
+  const uuid = () => crypto.randomUUID();
+  const { northwindEmployees } = useGetEmployees();
+
+  const columnBodyTemplate = (ctx: { dataContext: IgrCellTemplateContext }) => {
+    return (
+      <>
+        <p className={classes("typography__caption text")}>
+          <span>{ctx.dataContext.cell.value}</span>
+        </p>
+      </>
+    )
+  }
+
+  const columnBodyTemplate1 = (ctx: { dataContext: IgrCellTemplateContext }) => {
+    return (
+      <>
+        <div className={classes("column-layout group")}>
+          <p className={classes("typography__caption text")}>
+            <span>{ctx.dataContext.cell.value}</span>
+          </p>
+          <p className={classes("typography__body-1 text")}>
+            <span>{ctx.dataContext.cell.value}</span>
+          </p>
+        </div>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <div className={classes("row-layout master-view-container")}>
+        <IgrGrid data={northwindEmployees} primaryKey="employeeID" allowFiltering={true} filterMode="excelStyleFilter" className={classes("ig-typography ig-scrollbar grid")} key={uuid()}>
+          <IgrColumn field="employeeID" dataType="number" header="employeeID" sortable={true} selectable={false}></IgrColumn>
+          <IgrColumn field="lastName" dataType="string" header="lastName" sortable={true} selectable={false}></IgrColumn>
+          <IgrColumn field="firstName" dataType="string" header="firstName" sortable={true} bodyTemplate={columnBodyTemplate} selectable={false}></IgrColumn>
+          <IgrColumn field="title" dataType="string" header="title" sortable={true} bodyTemplate={columnBodyTemplate1} selectable={false}></IgrColumn>
+          <IgrColumn field="titleOfCourtesy" dataType="string" header="titleOfCourtesy" sortable={true} selectable={false}></IgrColumn>
+          <IgrColumn field="birthDate" dataType="date" header="birthDate" sortable={true} selectable={false}></IgrColumn>
+          <IgrColumn field="hireDate" dataType="date" header="hireDate" sortable={true} selectable={false}></IgrColumn>
+          <IgrColumn field="address.street" dataType="string" header="address street" sortable={true} selectable={false}></IgrColumn>
+          <IgrColumn field="address.city" dataType="string" header="address city" sortable={true} selectable={false}></IgrColumn>
+          <IgrColumn field="address.region" dataType="string" header="address region" sortable={true} selectable={false}></IgrColumn>
+          <IgrColumn field="address.postalCode" dataType="string" header="address postalCode" sortable={true} selectable={false}></IgrColumn>
+          <IgrColumn field="address.country" dataType="string" header="address country" sortable={true} selectable={false}></IgrColumn>
+          <IgrColumn field="address.phone" dataType="string" header="address phone" sortable={true} selectable={false}></IgrColumn>
+          <IgrColumn field="managerID" dataType="number" header="managerID" sortable={true} selectable={false}></IgrColumn>
+          <IgrColumn field="notes" dataType="string" header="notes" sortable={true} selectable={false}></IgrColumn>
+          <IgrColumn field="avatarUrl" dataType="string" header="avatarUrl" sortable={true} selectable={false}></IgrColumn>
+        </IgrGrid>
+      </div>
+    </>
+  );
+}
